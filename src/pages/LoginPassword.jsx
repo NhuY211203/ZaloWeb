f92b9321ae5fa83cd5a3df3784afe87734412995
TranKeTuanVuong <tuanvuongpokap@gmail.com>
2025-03-16 20:47:49 +0700

    api dang nhap

--- src/pages/LoginPassword.jsx
+++ src/pages/LoginPassword.jsx
@@ -2,9 +2,32 @@
 
 
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 const LoginPassword = () => {
   const navigate = useNavigate();
+  const [sdt, setSDT] = useState("");
+  const [matKhau, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  
+  const handleLogin = async (e) => {
+    e.preventDefault(); // Ngăn reload trang
+
+    try {
+      const response = await axios.post("http://localhost:5000/api/login", {
+        sdt,
+        matKhau,
+      });
+
+      console.log("Đăng nhập thành công!", response.data);
+      alert("Đăng nhập thành công!");
+      navigate("/forgot-password");
+    } catch (err) {
+      console.error("Lỗi đăng nhập:", err.response?.data || err.message);
+      setError("Email hoặc mật khẩu không đúng!");
+    }
+  };
 
   return (
     <div className="layout-container">
@@ -13,9 +36,10 @@ const LoginPassword = () => {
 
       <div className="container mt-4">
         <h2 className="text-lg font-bold mb-4">Đăng nhập với mật khẩu</h2>
-        <input type="text" placeholder="Số điện thoại" className="form-input" />
-        <input type="password" placeholder="Mật khẩu" className="form-input" />
-        <button className="btn-primary">Đăng nhập với mật khẩu</button>
+        <input type="text" placeholder="Số điện thoại" className="form-input" value={sdt} onChange={(e) => setSDT(e.target.value)}
+          required/>
+        <input type="password" placeholder="Mật khẩu" className="form-input" value={matKhau}  onChange={(e)=>setPassword(e.target.value)} />
+        <button className="btn-primary" onClick={handleLogin}>Đăng nhập với mật khẩu</button>
         <p className="text-blue-500 mt-4 text-center cursor-pointer" onClick={() => navigate("/forgot-password")}>Quên mật khẩu</p>
         <p className="text-gray-500 text-center cursor-pointer mt-2" onClick={() => navigate("/")}>Đăng nhập qua mã QR</p>
       </div>
